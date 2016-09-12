1898f0b41a93ccdf7bdd832c5f98c27320934189
igorcoding <igorcoding@gmail.com>
2016-09-13 01:27:51 +0300

    Move files around

--- provision/travis.sh
+++ provision/travis.sh
@@ -10,7 +10,27 @@ TestTarantool_LOCATION=/tmp/test-tarantool16.tar.gz
 wget ${TestTarantool_URL} -O ${TestTarantool_LOCATION}
 
 if [ -z "$TRAVIS_OS_NAME" ] || [ ${TRAVIS_OS_NAME} == 'linux' ]; then
-	source "${DIR}/tnt-cares.sh"
+	sudo apt-get install -y curl
+	curl https://packagecloud.io/tarantool/1_7/gpgkey | sudo apt-key add -
+
+	sudo apt-get -y install apt-transport-https
+	release=`lsb_release -c -s`
+
+	sudo rm -f /etc/apt/sources.list.d/*tarantool*.list
+	sudo bash -c 'cat > /etc/apt/sources.list.d/tarantool.list <<- EOF
+	deb https://packagecloud.io/tarantool/1_7/ubuntu/ `lsb_release -c -s` main
+	deb-src https://packagecloud.io/tarantool/1_7/ubuntu/ `lsb_release -c -s` main
+	EOF'
+
+	sudo apt-get update
+	sudo apt-get install -y tarantool
+
+	USR_SRC=/usr/local/src
+	wget http://c-ares.haxx.se/download/c-ares-1.10.0.tar.gz -O - | sudo tar -C ${USR_SRC} -xzvf -
+	cd ${USR_SRC}/c-ares-1.10.0
+	sudo ./configure
+	sudo make
+	sudo make install
 	
 elif [ ${TRAVIS_OS_NAME} == 'osx' ]; then
 	sudo sh -c 'echo "127.0.0.1 localhost" >> /etc/hosts'
@@ -20,14 +40,10 @@ elif [ ${TRAVIS_OS_NAME} == 'osx' ]; then
 	brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/cpanminus.rb
 	brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/c-ares.rb
 	cpanm --version
+	cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 	
-	# sudo cp $DIR/macos_ares_rules.h /usr/local/include/ares_rules.h
-	
-	brew update
 	brew install tarantool
 	tarantool -V
-	mkdir -p ~/Library/perl5/
-  	eval `perl -I ~/Library/perl5/lib/perl5 -Mlocal::lib=~/Library/perl5`
 	
 	# cat ~/.cpanm/work/**/*.log
 fi
